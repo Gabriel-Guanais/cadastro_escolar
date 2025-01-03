4e55d82d3f528f3770411767ce6dd24ff811e1eb
Gabriel-Guanais <laiteguanais@gmail.com>
2025-01-02 22:37:28 -0400

    Add iformations and fix the menu table

--- cadastro/controle.py
+++ cadastro/controle.py
@@ -3,19 +3,20 @@
 ciano = "\033[96m"
 reset = "\033[0m"
 def menu():
-    print(f'=============={ciano}IFMS{reset}===============')
-    print(f"| [{ciano}1{reset}] Cadastro Aluno             |")
-    print(f"| [{ciano}2{reset}] Cadastrar Professor        |")
-    print(f"| [{ciano}3{reset}] Cadastrar Disciplina       |")
-    print(f"| [{ciano}4{reset}] Cadastrar Turma            |")
-    print(f"| [{ciano}9{reset}] Matricular Aluno em Turma  |")
-    print(f"| [{ciano}6{reset}] Listar Disciplinas         |")
-    print(f"|[{ciano}10{reset}] Alocar Diciplina em Turma  |")
-    print(f"| [{ciano}4{reset}] Listar Professores         |")
-    print(f"| [{ciano}8{reset}] Listar Turma               |")
-    print(f"| [{ciano}2{reset}] Listar Aluno               |")
-    print(f"| [0] Sair                                     |")
-    print("================================================")
+    print(f'======================{ciano}IFMS{reset}=======================')
+    print(f"| [{ciano}1{reset}] Cadastro Aluno                             |")
+    print(f"| [{ciano}2{reset}] Cadastrar Professor                        |")
+    print(f"| [{ciano}3{reset}] Cadastrar Disciplina                       |")
+    print(f"| [{ciano}4{reset}] Cadastrar Turma                            |")
+    print(f"| [{ciano}5{reset}] Matricular Aluno em Turma                  |")
+    print(f"| [{ciano}6{reset}] Alocar Professor em diciplinas             |")
+    print(f"| [{ciano}7{reset}] Alocar Diciplina em Turma                  |")
+    print(f"| [{ciano}8{reset}] Filtrar Professores por Diciplina          |")
+    print(f"| [{ciano}9{reset}] Listar Alunos Matriculado em Turmas        |")
+    print(f"|[{ciano}10{reset}] Listar Professores alocados em disciplinas |")
+    print(f"|[{ciano}11{reset}] Listar Diciplinas alocadas em turmas       |")
+    print(f"| [{ciano}0{reset}]  Sair                                      |")
+    print("=================================================================")
 
     try:
         escolha_usr = int(input(f"{ciano}Insira a Opção: {reset}"))
