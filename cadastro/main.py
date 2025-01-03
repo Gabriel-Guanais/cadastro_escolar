461d7fb5e751593ba29ae87aeb4670ae7128d00c
Gabriel-Guanais <laiteguanais@gmail.com>
2025-01-02 21:00:28 -0400

    Add elif conditions with screen-clearing for user actions

--- cadastro/main.py
+++ cadastro/main.py
@@ -1,6 +1,7 @@
 from controle import resposta_usr
 from aluno import cadastrar_aluno
 from professor import cadastrar_professor 
+from diciplina import cadastrar_diciplina
 import os
 
 def limpar_tela():
@@ -13,6 +14,23 @@ if resposta_usr == 1:
 elif resposta_usr == 2:
     limpar_tela()
     cadastrar_professor()
+
+elif resposta_usr == 3:
+    limpar_tela()
+    cadastrar_diciplina()
+    
+elif resposta_usr == 4:
+    limpar_tela()
+    ...
+    
+elif resposta_usr == 5:
+    limpar_tela()
+    ...
+    
+elif resposta_usr == 6:
+    limpar_tela
+    ...
+    
     
 else:
     print("funcionou")
